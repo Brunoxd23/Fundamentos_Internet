75b89e0eef8b0265d162ae68ca4b146403e8da4c
Bruno Monteiro <140560618+Brunoxd23@users.noreply.github.com>
2025-03-24 16:55:28 -0300

    Atualizar o page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 import Topics from "../components/Topics";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
-      <footer />
+      <Footer />
     </main>
   );
 }
